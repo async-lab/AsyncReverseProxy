86d14a1fa9b6fc2af4d7a13432851b3d688b631f
dsx137 <1419696375@qq.com>
2024-12-02 04:01:45 +0800

    fix: buf from pool

--- pkg/comm/comm.go
+++ pkg/comm/comm.go
@@ -7,12 +7,11 @@ import (
 	"sync"
 
 	"club.asynclab/asrp/pkg/packet"
-
 )
 
 var bufPool = sync.Pool{
 	New: func() interface{} {
-		buf := make([]byte, 32*1024*1024)
+		buf := make([]byte, 4+32*1024*1024)
 		return &buf
 	},
 }
@@ -22,11 +21,15 @@ func SendPacket(conn net.Conn, p packet.IPacket) (int, error) {
 	if err != nil {
 		return 0, err
 	}
+	bufPtr := bufPool.Get().(*[]byte)
+	defer bufPool.Put(bufPtr)
+	buf := *bufPtr
+
 	length := uint32(len(data))
-	buffer := make([]byte, 4+length)
-	binary.BigEndian.PutUint32(buffer[:4], length)
-	copy(buffer[4:], data)
-	return conn.Write(buffer)
+	buf = buf[:4+length]
+	binary.BigEndian.PutUint32(buf[:4], length)
+	copy(buf[4:], data)
+	return conn.Write(buf)
 }
 
 func ReceivePacket(conn net.Conn) (packet.IPacket, error) {
@@ -35,10 +38,12 @@ func ReceivePacket(conn net.Conn) (packet.IPacket, error) {
 	if err != nil {
 		return nil, err
 	}
+	bufPtr := bufPool.Get().(*[]byte)
+	defer bufPool.Put(bufPtr)
+	buf := *bufPtr
 
 	length := binary.BigEndian.Uint32(lengthBuf)
-
-	buf := make([]byte, length)
+	buf = buf[:length]
 	n, err := io.ReadFull(conn, buf)
 	if err != nil {
 		return nil, err
@@ -53,8 +58,9 @@ func ReceivePacket(conn net.Conn) (packet.IPacket, error) {
 
 func ReadForBytes(conn net.Conn) ([]byte, error) {
 	bufPtr := bufPool.Get().(*[]byte)
-	buf := *bufPtr
 	defer bufPool.Put(bufPtr)
+	buf := *bufPtr
+
 	n, err := conn.Read(buf)
 	if err != nil {
 		return nil, err
